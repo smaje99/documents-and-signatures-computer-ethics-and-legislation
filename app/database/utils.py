7abaa7b59ddbcedc4170ab5a189dcf4501acf70b
Sergio Majé <smajefranco@gmail.com>
2025-06-23 06:30:17 -0500

    ♻️ Refactor enum_values_callable util
    Refactor enum_values_callable to improve type hinting and correct enum value extraction

--- app/database/utils.py
+++ app/database/utils.py
@@ -1,5 +1,9 @@
+from enum import Enum
+
+
 __all__ = ('enum_values_callable',)
 
-def enum_values_callable(obj):
+
+def enum_values_callable(obj: type[Enum]) -> list[str]:
   '''Extracts the values of an enum object.'''
-  return [e.value for e in obj]
+  return [e.value for e in obj.__members__.values()]
